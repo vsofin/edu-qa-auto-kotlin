9dc8a229e3a8bd70423d27630942fe36d92558b5
Владимир Софин <vsofin@ya.ru>
2022-06-17 09:52:19 +0300

    Delete firstAlmagul.kts
    remove firstAlmagul

--- src/main/kotlin/firstAlmagul.kts
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-
-val minutes = LocalDateTime.now().format(DateTimeFormatter.ofPattern("mm")).toByte() //перевожу в численный тип
-
-if (minutes % 2 != 0) //остаток от деления минуты на 2 сравниваю с 0
-    println("Сейчас значение минут нечетное: " + minutes)
-else
-    println("Сейчас значение минут четное: " + minutes)
